ab6a7a5adffaea63aa308b6a5d0f5f07a8d99040
Bonnoyugi <3202558027@qq.com>
2020-05-17 14:17:03 +0800

    commit Homework10

---
+++ Homework10/Crawler/Crawler.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Crawler
+{
+    class Crawler
+    {
+        //
+        public event Action<Crawler, int, string, string> PageDownloaded;
+        //targetUrls主要的作用就是防止重复爬取网页，记录已经爬取过的网页
+        private ConcurrentDictionary<string, bool> targetUrls = new ConcurrentDictionary<string, bool>();
+
+        private ConcurrentQueue<string> pending = new ConcurrentQueue<string>();
+
+        private static readonly string urlDetectRegex = @"(href|HREF)\s*=\s*[""'](?<url>[^""'#>]+)[""']";
+
+        public static readonly string urlParseRegex = @"^(?<site>https?://(?<host>[\w\d.]+)(:\d+)?($|/))([\w\d]+/)*(?<file>[^#?]*)";
+
+        public string startUrl;
+
+        private int index = 0;
+
+        public Crawler()
+        {
+
+        }
+
+        public void Start()
+        {
+            targetUrls.Clear();
+            pending.Enqueue(startUrl);
+            List<Task> tasks = new List<Task>();
+            index = 1;
+
+            while(pending.TryDequeue(out string url))
+            {
+                Task task = Task.Run(() => DownloadAndParse(url));
+                tasks.Add(task);
+            }
+
+            Task.WaitAll(tasks.ToArray());
+        }
+
+        private void DownloadAndParse(string url)
+        {
+            try
+            {
+                string html = Download(url);
+                targetUrls[url] = true;
+                Parse(html, url);
+            }catch(Exception e)
+            {
+                PageDownloaded(this, -1, url, "failed");//索引应当按照老师的做法直接在参数中给明才好
+            }
+        }
+
+        private string Download(string url)
+        {
+            WebClient webClient = new WebClient();
+            string html = webClient.DownloadString(url);
+            File.WriteAllText(index+".html", html);
+            PageDownloaded(this, index, url, "success");
+            index++;
+            return html;
+        }
+
+        //分析url检查是不是html
+        private void Parse(string html, string pageUrl)
+        {
+            MatchCollection matches = new Regex(urlDetectRegex).Matches(html);
+            foreach(Match match in matches)
+            {
+                string url = match.Groups["url"].Value;
+                Match urlMatch = new Regex(urlParseRegex).Match(url);
+                string host = urlMatch.Groups["host"].Value;
+                string file = urlMatch.Groups["file"].Value;
+                if (file == "") file = "index.html";
+                if (Regex.IsMatch(file, @".html?$") && !targetUrls.ContainsKey(url))
+                {
+                    pending.Enqueue(url);
+                    targetUrls.TryAdd(url, false);
+                }
+            }
+        }
+
+        //static private string FixUrl();
+    }
+}
---
+++ Homework10/Crawler/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Crawler
+{
+    public partial class Form1 : Form
+    {
+        Crawler crawler = new Crawler();
+        Thread thread = null;
+        public Form1()
+        {
+            InitializeComponent();
+            crawler.PageDownloaded += Crawler_PageDownloaded;
+        }
+
+        //重要！利用委托机制实现逻辑与窗体的分离，利用invoke处理不同的进程对窗体控件的处理
+        private void Crawler_PageDownloaded(Crawler crawler, int index, string url, string info)
+        {
+            var pageInfo = new { Index = index, URL = url, Status = info };
+            Action action = () => { bindingSource1.Add(pageInfo); };
+            if (this.InvokeRequired)
+            {
+                this.Invoke(action);
+            }
+            else
+            {
+                action();
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            bindingSource1.Clear();
+            crawler.startUrl = textBox1.Text;
+            if (thread != null)
+            {
+                thread.Abort();
+            }
+            thread = new Thread(crawler.Start);
+            thread.Start();
+        }
+    }
+}
